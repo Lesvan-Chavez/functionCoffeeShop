090c97d2ed492df2f221ae5a27ae6453b594bfe5
Lesvan Chavez <lesvanrealm@gmail.com>
2025-03-17 19:50:18 -0500

    End of login
    Part 1 of class

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-const user = { username: 'admin', password: 'password123' }
+const user = { username: "admin", password: "password123" };
 
 let loggedInUser = null;
 
 function handleLogin(event) {
     event.preventDefault();
-    const usernameElement = document.getElementById('username').value;
-    const passwordElement = document.getElementById('password').value;
+    const usernameElement = document.getElementById("username").value;
+    const passwordElement = document.getElementById("password").value;
 
-    if(user.username !== usernameElement || user.password !== passwordElement) {
+    if (
+        user.username !== usernameElement ||
+        user.password !== passwordElement
+    ) {
         alert(`User or Password not found`);
-        return
-    }
-    else{
+        return;
+    } else {
         loggedInUser = user;
-
     }
 
-    let welcomeMessageElement = document.getElementById('welcome-message');
+    let welcomeMessageElement = document.getElementById("welcome-message");
     welcomeMessageElement.innerText = `Welcome ${loggedInUser.username}`;
-
-
 }
 
+function addToCart(item, price) {}
