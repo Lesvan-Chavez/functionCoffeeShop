99187fcf84c72be93f9791aae5bd94c58a9fe1a9
Lesvan Chavez <lesvanrealm@gmail.com>
2025-03-18 20:13:59 -0500

    Check out update
    Adding the total of the items to the cart and attempting to write out the quantity of items

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const user = { username: "admin", password: "password123" };
 
 let loggedInUser = null;
 
+const cart = [];
+
 function handleLogin(event) {
     event.preventDefault();
     const usernameElement = document.getElementById("username").value;
@@ -21,4 +23,47 @@ function handleLogin(event) {
     welcomeMessageElement.innerText = `Welcome ${loggedInUser.username}`;
 }
 
-function addToCart(item, price) {}
+function addToCart(item, price) {
+    let totalCost = 0;
+    console.log(`Item:`, item); //Testing functionality with console
+    console.log(`Price:`, price);
+
+    if (cart.length === 0) {
+        const objectToInsert = {
+            drink: item,
+            cost: price,
+            quanity: 1,
+        };
+        cart.push(objectToInsert);
+    } else {
+        for (let index = 0; index < cart.length; index++) {
+            if (cart[index].drink === item) {
+                cart[index].quanity++;
+            } else {
+                const objectToInsert = {
+                    drink: item,
+                    cost: price,
+                    quanity: 1,
+                };
+                cart.push(objectToInsert);
+            }
+        }
+    }
+
+    let ulElement = document.getElementById("cart-items");
+    let liElement = document.createElement("li");
+
+    for (let index = 0; index < cart.length; index++) {
+        console.log(`addedCartItem`, cart[index]);
+
+        totalCost += cart[index].cost;
+
+        liElement.innerText = `Item: ${cart[index].drink}, Price: $${cart[
+            index
+        ].cost.toFixed(2)}`;
+        ulElement.appendChild(liElement);
+    }
+
+    let totalElement = document.getElementById("total");
+    totalElement.innerText = totalCost.toFixed(2);
+}
